8a2c72b02e9c01cf6e8cec144fdf96efced3addf
Noya Hezkia <noya.hezkia@gmail.com>
2025-12-16 16:35:45 +0200

    Show RAM and price relationship

--- miniproj1.py
+++ miniproj1.py
@@ -10,10 +10,26 @@ def plot_column_histogram(dataset, column_name, axes_name, plot_name):
     plt.title(plot_name)
     plt.show() 
 
+# Detect a column outliers using IQR formula
+def detect_iqr_outliers(dataset, column_name):
+    q1 = dataset[column_name].quantile(0.25)
+    q3 = dataset[column_name].quantile(0.75)
+    iqr = q3 - q1
+
+    lower_bound = q1 - 1.5 * iqr
+    upper_bound = q3 + 1.5 * iqr
+
+    # Filter the dataset for only outliers
+    outliers = dataset[(dataset[column_name] < lower_bound) 
+                       | (dataset[column_name] > upper_bound)]
+    
+    return outliers
+
 laptop_data = pd.read_csv('laptop_price - dataset.csv')
 price_column = 'Price (Euro)'
 company_column = 'Company'
 os_column = 'OpSys'
+ram_column = 'RAM (GB)'
 
 # Plot a histogram of all the prices
 plot_column_histogram(laptop_data, price_column, price_column, 'Price Distribution')
@@ -47,9 +63,33 @@ print(*unique_os, sep='\n')
 # Plot price distributions for each OS type
 # Create a seperate histogram of the prices for each OS type 
 subplots_grid = sns.FacetGrid(laptop_data, col='UniformOpSys', col_wrap=3, sharex=False)
-subplots_grid.map_dataframe(sns.histplot, x=price_column, color="skyblue", edgecolor="black")
+subplots_grid.map_dataframe(sns.histplot, x=price_column, color='purple', edgecolor='black')
 subplots_grid.set_axis_labels(price_column, 'Count')
 subplots_grid.set_titles('Price Distribution for {col_name}')
 plt.tight_layout()
 plt.show()
 
+# Show the relationship between RAM and price by scatter plot and regression line
+# Starting by finding pearson correlation coefficient
+correlation = laptop_data[ram_column].corr(laptop_data[price_column])
+
+# Plot the relationship graph
+plt.figure()
+plt.text(
+    0.05, 0.95,
+    f'Correlation (Pearson): {correlation:.2f}',
+    transform=plt.gca().transAxes,
+    verticalalignment='top'
+)
+sns.regplot(x=ram_column, y=price_column, data=laptop_data, scatter_kws={'color':'skyblue', 's':60, 'alpha':0.7},
+            line_kws={'color':'red', 'linewidth':2})
+plt.xlabel(ram_column)
+plt.ylabel(price_column)
+plt.title('Relationship between RAM and Laptop Price')
+plt.show()
+
+# Calculate and show price outliers 
+price_outliers = detect_iqr_outliers(laptop_data, price_column)
+
+print('Price outliers:')
+print(price_outliers)
